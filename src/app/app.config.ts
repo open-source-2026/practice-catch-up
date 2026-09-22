be3e8d2ffc5998d5493abac03d5700c6e774fd4f
Yamil Tejada <yamil.tejada9.pumacayo10@gmail.com>
2026-09-22 17:33:22 -0500

    feat(config): add HttpClient provider for improved API communication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideTranslateService} from '@ngx-translate/core';
 import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(),
     provideTranslateService({
 
       loader: provideTranslateHttpLoader({prefix: './i18n/', suffix: '.json'}),
